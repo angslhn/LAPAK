0266d68d731e8372ee93b80b545a6fc43fa18c54
Aang Solihin <angslhn@gmail.com>
2026-05-31 09:39:24 +0700

    Menambahkan konfigurasi mysql

---
+++ backend/lib/mysql.js
@@ -0,0 +1,53 @@
+const mysql = require('mysql2/promise');
+
+const {
+  isProduction,
+  dbHost,
+  dbUser,
+  dbName,
+  dbPassword,
+  dbPort,
+} = require('../config/env');
+
+let pool;
+
+const getPool = () => {
+  if (!pool) {
+    pool = mysql.createPool({
+      host: dbHost,
+      user: dbUser,
+      database: dbName,
+      password: dbPassword,
+      port: dbPort,
+      waitForConnections: true,
+      connectionLimit: isProduction ? 1 : 10,
+      maxIdle: isProduction ? 1 : 10,
+      idleTimeout: isProduction ? 10000 : 60000,
+      queueLimit: 0,
+      enableKeepAlive: !isProduction,
+      keepAliveInitialDelay: 0,
+      ...(isProduction && {
+        ssl: { rejectUnauthorized: true },
+      }),
+    });
+  }
+
+  return pool;
+};
+
+const connectDatabase = async () => {
+  const pool = getPool();
+
+  let connection;
+
+  try {
+    connection = await pool.getConnection();
+    console.info('[DATABASE] Berhasil terhubung');
+  } catch {
+    console.info('[DATABASE] Gagal terhubung');
+  } finally {
+    connection?.release();
+  }
+};
+
+module.exports = { getPool, connectDatabase };
