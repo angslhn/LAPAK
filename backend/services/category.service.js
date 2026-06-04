18765840fb43d6763dd45424b1faa2a99e267afc
Aang Solihin <angslhn@gmail.com>
2026-06-05 06:22:18 +0700

    Menambahkan category service

---
+++ backend/services/category.service.js
@@ -0,0 +1,49 @@
+const CategoryModel = require('../models/category.model');
+
+const {
+  CATEGORY_ALREADY_EXISTS,
+  CATEGORY_NOT_FOUND,
+  CATEGORY_UPDATE_FAILED,
+} = require('../helpers/errorCodes');
+
+const getAll = async () => {
+  try {
+    return await CategoryModel.findAll();
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const create = async (data) => {
+  try {
+    const { name } = data;
+
+    const category = await CategoryModel.findByName(name);
+
+    if (category) throw new Error(CATEGORY_ALREADY_EXISTS);
+
+    return await CategoryModel.create(data);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const update = async (data) => {
+  try {
+    const { id, name } = data;
+
+    const category = await CategoryModel.findById(id);
+
+    if (!category) throw new Error(CATEGORY_NOT_FOUND);
+
+    const result = await CategoryModel.update({ id, name });
+
+    if (result === 0) throw new Error(CATEGORY_UPDATE_FAILED);
+
+    return result;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+module.exports = { getAll, create, update };
