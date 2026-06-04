42b9a63bc132bc08c913c397f32d81619259e158
Aang Solihin <angslhn@gmail.com>
2026-06-04 20:12:01 +0700

    Menambahkan fungsi pembuatan kode invoice setiap transaksi

---
+++ backend/helpers/invoice.js
@@ -0,0 +1,14 @@
+const makeInvoiceCode = (seq) => {
+  const now = new Date();
+
+  const dateStr =
+    now.getFullYear() +
+    String(now.getMonth() + 1).padStart(2, '0') +
+    String(now.getDate()).padStart(2, '0');
+
+  const seqStr = String(seq).padStart(4, '0');
+
+  return `LPK/${dateStr}/${seqStr}`;
+};
+
+module.exports = { makeInvoiceCode };
