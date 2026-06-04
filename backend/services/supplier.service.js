30a02658447c58aa07139046f47dd587468c194c
Aang Solihin <angslhn@gmail.com>
2026-06-05 06:26:22 +0700

    Menambahkan supplier service

---
+++ backend/services/supplier.service.js
@@ -0,0 +1,67 @@
+const SupplierModel = require('../models/supplier.model');
+
+const { SUPPLIER_NOT_FOUND } = require('../helpers/error_codes');
+
+const getAll = async () => {
+  try {
+    return await SupplierModel.findAll();
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const getById = async (data) => {
+  try {
+    const { id } = data;
+
+    const supplier = await SupplierModel.findById(id);
+
+    if (!supplier) throw new Error(SUPPLIER_NOT_FOUND);
+
+    return supplier;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const create = async (data) => {
+  try {
+    const { name, phone, address } = data;
+
+    const supplierId = await SupplierModel.create({ name, phone, address });
+
+    return supplierId;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const update = async (data) => {
+  try {
+    const { id, ...fields } = data;
+
+    const supplier = await SupplierModel.findById(id);
+
+    if (!supplier) throw new Error(SUPPLIER_NOT_FOUND);
+
+    await SupplierModel.update({ id, ...fields });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const remove = async (data) => {
+  try {
+    const { id } = data;
+
+    const supplier = await SupplierModel.findById(id);
+
+    if (!supplier) throw new Error(SUPPLIER_NOT_FOUND);
+
+    await SupplierModel.remove(id);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+module.exports = { getAll, getById, create, update, remove };
