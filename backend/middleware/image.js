2f3de2f02c1334fb69e4573d003f930d677d1c24
Aang Solihin <angslhn@gmail.com>
2026-06-05 19:16:28 +0700

    Menambahkan middleware upload gambar

---
+++ backend/middleware/image.js
@@ -0,0 +1,67 @@
+const multer = require('multer');
+
+const { error } = require('../helpers/response');
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (_req, file, cb) => {
+  const allowedMimeTypes = [
+    'image/jpeg',
+    'image/jpg',
+    'image/png',
+    'image/webp',
+  ];
+
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error('INVALID_FILE_FORMAT'), false);
+  }
+};
+
+const mediaUploader = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 1024 * 1024 * 4, // 3MB
+  },
+});
+
+const image = (req, res, next) => {
+  const imageMiddleware = mediaUploader.single('image');
+
+  imageMiddleware(req, res, (err) => {
+    if (err) {
+      if (err.message === 'INVALID_FILE_FORMAT') {
+        return error(
+          res,
+          'INVALID_FILE_FORMAT',
+          'File format not supported. Use JPG/PNG/WEBP',
+          400
+        );
+      }
+
+      if (err instanceof multer.MulterError) {
+        if (err.code === 'LIMIT_FILE_SIZE') {
+          return error(
+            res,
+            'FILE_TOO_LARGE',
+            'File size is too large. Maximum 3MB',
+            413
+          );
+        }
+      }
+
+      return error(
+        res,
+        'IMAGE_UPLOAD_ERROR',
+        'An error occurred while uploading the image',
+        500
+      );
+    }
+
+    next();
+  });
+};
+
+module.exports = { image };
