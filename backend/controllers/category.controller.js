ae0bf53a6c7f272b154815463545e8bb5f4706b8
Aang Solihin <angslhn@gmail.com>
2026-06-06 08:20:35 +0700

    Menambahkan category controller

---
+++ backend/controllers/category.controller.js
@@ -0,0 +1,68 @@
+const CategoryService = require('../services/category.service');
+
+const { ok, error, created } = require('../helpers/response');
+
+const { ERROR_MESSAGES } = require('../helpers/error_messages');
+const { ERROR_STATUS } = require('../helpers/error_status');
+
+const getAllHandler = async (req, res) => {
+  try {
+    const data = await CategoryService.getAll();
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const createHandler = async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    const data = await CategoryService.create({ name });
+
+    return created(res, data, 'Kategori baru berhasil dibuat');
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const updateHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    const data = await CategoryService.update({ id, name });
+
+    return ok(res, data, 'Kategori berhasil diperbarui');
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
