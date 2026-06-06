dc1d63d9ce8723258eec3b9faf04413bbe2549e0
Aang Solihin <angslhn@gmail.com>
2026-06-06 19:41:07 +0700

    Menambahkan stock controller

---
+++ backend/controllers/stock.controller.js
@@ -0,0 +1,114 @@
+const StockService = require('../services/stock.service');
+
+const { ok, error, created } = require('../helpers/response');
+
+const { ERROR_MESSAGES } = require('../helpers/error_messages');
+const { ERROR_STATUS } = require('../helpers/error_status');
+
+const getAllHandler = async (req, res) => {
+  try {
+    const data = await StockService.getAll();
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const getLowStockHandler = async (req, res) => {
+  try {
+    const data = await StockService.getLowStock();
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const adjustStockHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { type, quantity, note } = req.body;
+
+    const data = await StockService.adjustStock({ id, type, quantity, note });
+
+    return ok(res, data, 'Stok produk berhasil diubah');
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const getMutationsHandler = async (req, res) => {
+  try {
+    const data = await StockService.getMutations();
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const getMutationsByProductHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = await StockService.getMutationsByProduct({ id });
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+module.exports = {
+  getAllHandler,
+  getLowStockHandler,
+  adjustStockHandler,
+  getMutationsHandler,
+  getMutationsByProductHandler,
+};
