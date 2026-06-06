d53ca925ce5c97f03b3b371de84db3556ab559b0
Aang Solihin <angslhn@gmail.com>
2026-06-06 21:04:48 +0700

    Menambahkan purchase controller

---
+++ backend/controllers/purchase.controller.js
@@ -0,0 +1,103 @@
+const PurchaseService = require('../services/purchase.service');
+
+const { ok, error, created } = require('../helpers/response');
+
+const { ERROR_MESSAGES } = require('../helpers/error_messages');
+const { ERROR_STATUS } = require('../helpers/error_status');
+
+const getAllHandler = async (req, res) => {
+  try {
+    const data = await PurchaseService.getAll();
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const getByIdHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = await PurchaseService.getById({ id });
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const createHandler = async (req, res) => {
+  try {
+    const { supplier_id, receipt_number, date, due_date, items, note } =
+      req.body;
+
+    const data = await PurchaseService.create({
+      supplier_id,
+      receipt_number,
+      date,
+      due_date,
+      items,
+      note,
+    });
+
+    return created(res, data, 'Pembelian berhasil dibuat');
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const markAsPaidHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = await PurchaseService.markAsPaid({ id });
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+module.exports = {
+  getAllHandler,
+  getByIdHandler,
+  createHandler,
+  markAsPaidHandler,
+};
