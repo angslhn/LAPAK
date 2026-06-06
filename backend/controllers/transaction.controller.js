e49ea8c91644365fe5e36691b6bfb5dd4da384d6
Aang Solihin <angslhn@gmail.com>
2026-06-06 19:59:21 +0700

    Menambahkan transaction controller

---
+++ backend/controllers/transaction.controller.js
@@ -0,0 +1,114 @@
+const TransactionService = require('../services/transaction.service');
+
+const { ok, error, created } = require('../helpers/response');
+
+const { ERROR_MESSAGES } = require('../helpers/error_messages');
+const { ERROR_STATUS } = require('../helpers/error_status');
+
+const getAllHandler = async (req, res) => {
+  try {
+    const data = await TransactionService.getAll();
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const getByIdHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = await TransactionService.getById({ id });
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const createHandler = async (req, res) => {
+  try {
+    const { id } = req.user;
+
+    const {
+      customer_id,
+      payment_method,
+      items,
+      discount,
+      tax,
+      due_date,
+      note,
+    } = req.body;
+
+    const data = await TransactionService.create({
+      user_id: id,
+      customer_id,
+      payment_method,
+      items,
+      discount,
+      tax,
+      due_date,
+      note,
+    });
+
+    return created(res, data, 'Transaksi berhasil dibuat');
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const cancelHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = await TransactionService.cancel({ id });
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+module.exports = {
+  getAllHandler,
+  getByIdHandler,
+  createHandler,
+  cancelHandler,
+};
