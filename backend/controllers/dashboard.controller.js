894ad912f5df51f0acee9da327520d76d88b59d5
Aang Solihin <angslhn@gmail.com>
2026-06-06 08:20:55 +0700

    Menambahkan dashboard controller

---
+++ backend/controllers/dashboard.controller.js
@@ -0,0 +1,27 @@
+const DashboardService = require('../services/dashboard.service');
+
+const { ok, error } = require('../helpers/response');
+
+const { ERROR_MESSAGES } = require('../helpers/error_messages');
+const { ERROR_STATUS } = require('../helpers/error_status');
+
+const getSummaryHandler = async (req, res) => {
+  try {
+    const data = await DashboardService.getSummary();
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+module.exports = { getSummaryHandler };
