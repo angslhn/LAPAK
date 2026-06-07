7a2117662dee9f44b4d39225a811622f529dfbc1
Aang Solihin <angslhn@gmail.com>
2026-06-07 18:35:58 +0700

    Menambahkan debt controller

---
+++ backend/controllers/debt.controller.js
@@ -0,0 +1,93 @@
+const DebtService = require('../services/debt.service');
+
+const { ok, error } = require('../helpers/response');
+
+const { ERROR_MESSAGES } = require('../helpers/error_messages');
+const { ERROR_STATUS } = require('../helpers/error_status');
+
+const getCustomerDebtsHandler = async (req, res) => {
+  try {
+    const data = await DebtService.getCustomerDebts();
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const payCustomerDebtHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = await DebtService.payCustomerDebt({ id });
+
+    return ok(res, data, 'Piutang berhasil dibayar');
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const getSupplierDebtsHandler = async (req, res) => {
+  try {
+    const data = await DebtService.getSupplierDebts();
+
+    return ok(res, data);
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+const paySupplierDebtHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = await DebtService.paySupplierDebt({ id });
+
+    return ok(res, data, 'Hutang berhasil dibayar');
+  } catch (err) {
+    let code = err.message;
+
+    if (!ERROR_MESSAGES[code]) {
+      code = 'INTERNAL_SERVER_ERROR';
+    }
+
+    const message = ERROR_MESSAGES[code];
+    const httpStatus = ERROR_STATUS[code];
+
+    return error(res, code, message, httpStatus);
+  }
+};
+
+module.exports = {
+  getCustomerDebtsHandler,
+  payCustomerDebtHandler,
+  getSupplierDebtsHandler,
+  paySupplierDebtHandler,
+};
