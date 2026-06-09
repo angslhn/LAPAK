6ea8dbfce5bc58201ee45bee88f46c58f8a02022
Aang Solihin <angslhn@gmail.com>
2026-06-09 20:04:19 +0700

    Menambahkan route baru untuk proses OAuth Google

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,4 +1,12 @@
 const router = require('express').Router();
+const passport = require('../lib/passport');
+
+const {
+  authCookieName,
+  isProduction,
+  cookieMaxAge,
+  clientURL,
+} = require('../config/env');
 
 const {
   loginHandler,
@@ -7,6 +15,30 @@ const {
   resetPasswordHandler,
   logoutHandler,
 } = require('../controllers/auth.controller');
+
+router.get(
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+router.get(
+  '/google/callback',
+  passport.authenticate('google', {
+    session: false,
+    failureRedirect: clientURL + '/masuk?error=oauth_failed',
+  }),
+  (req, res) => {
+    const { token } = req.user;
+
+    res.cookie(authCookieName, token, {
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: 'strict',
+      maxAge: cookieMaxAge,
+    });
+
+    res.redirect(clientURL + '/beranda');
+  }
+);
 
 router.post('/login', loginHandler);
 router.post('/register', registerHandler);
