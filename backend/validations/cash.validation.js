ff115b6a1e996122b59a94ab8840f95fbe51d82f
Aang Solihin <angslhn@gmail.com>
2026-06-11 20:06:31 +0700

    Menambahkan cash validation

---
+++ backend/validations/cash.validation.js
@@ -0,0 +1,14 @@
+const CashInput = require('./inputs/cash.input');
+
+const create = (data) => {
+  const { date, amount, note } = data;
+
+  const checkDate = CashInput.date(date);
+  if (checkDate) throw new Error(checkDate);
+
+  const checkAmount = CashInput.amount(amount);
+  if (checkAmount) throw new Error(checkAmount);
+
+  const checkNote = CashInput.note(note);
+  if (checkNote) throw new Error(checkNote);
+};
