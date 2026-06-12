c0393e84664e47e38725bb95ff3c0c59e8cd8958
Aang Solihin <angslhn@gmail.com>
2026-06-12 13:18:13 +0700

    Perubahan nama variabel dan menambahkan export fungsi

--- backend/validations/category.validation.js
+++ backend/validations/category.validation.js
@@ -3,15 +3,17 @@ const CategoryInput = require('./inputs/category.input');
 const create = (data) => {
   const { name } = data;
 
-  const checkCategory = CategoryInput.category(name);
-  if (checkCategory) throw new Error(checkCategory);
+  const checkName = CategoryInput.category(name);
+  if (checkName) throw new Error(checkName);
 };
 
 const update = (data) => {
   const { id } = data;
 
-  const checkCategoryId = CategoryInput.categoryId(id);
-  if (checkCategoryId) throw new Error(checkCategoryId);
+  const checkId = CategoryInput.id(id);
+  if (checkId) throw new Error(checkId);
 
   create(data);
 };
+
+module.exports = { create, update };
