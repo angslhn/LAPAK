359c42715aec9fa6253fd2277c2a7898236b2685
Aang Solihin <angslhn@gmail.com>
2026-06-12 13:19:30 +0700

    Menambahkan purchase validation

---
+++ backend/validations/purchase.validation.js
@@ -0,0 +1,22 @@
+const PurchaseInput = require('./inputs/purchase.input');
+
+const create = (data) => {
+  const { supplier_id, receipt_number, date, due_date, items, note } = data;
+
+  const checkSupplierId = PurchaseInput.supplierId(supplier_id);
+  if (checkSupplierId) throw new Error(checkSupplierId);
+
+  const checkDate = PurchaseInput.date(date);
+  if (checkDate) throw new Error(checkDate);
+
+  const checkDueDate = PurchaseInput.dueDate(due_date);
+  if (checkDueDate) throw new Error(checkDueDate);
+
+  const checkItems = PurchaseInput.items(items);
+  if (checkItems) throw new Error(checkItems);
+
+  const checkNote = PurchaseInput.note(note);
+  if (checkNote) throw new Error(checkNote);
+};
+
+module.exports = { create };
