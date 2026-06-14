821f7e74bd4bafb1df13819ea40be5f4ba6767b5
Aang Solihin <angslhn@gmail.com>
2026-06-14 17:11:54 +0700

    Perbaikan pada fungsi validasi produk terlaris

--- backend/validations/report.validation.js
+++ backend/validations/report.validation.js
@@ -19,19 +19,24 @@ const revenue = (data) => {
 };
 
 const topProduct = (data) => {
-  const { limit, from, to } = data;
+  const { limit, period, from, to } = data;
 
   const checkLimit = ReportInput.limit(limit);
   if (checkLimit) throw new Error(checkLimit);
 
-  const checkFromDate = ReportInput.fromDate(from);
-  if (checkFromDate) throw new Error(checkFromDate);
+  const checkPeriod = ReportInput.period(period);
+  if (checkPeriod) throw new Error(checkPeriod);
 
-  const checkToDate = ReportInput.toDate(to);
-  if (checkToDate) throw new Error(checkToDate);
+  if (period === 'custom') {
+    const checkFromDate = ReportInput.fromDate(from);
+    if (checkFromDate) throw new Error(checkFromDate);
 
-  const checkRangeDate = ReportInput.rangeDate(from, to);
-  if (checkRangeDate) throw new Error(checkRangeDate);
+    const checkToDate = ReportInput.toDate(to);
+    if (checkToDate) throw new Error(checkToDate);
+
+    const checkRangeDate = ReportInput.rangeDate(from, to);
+    if (checkRangeDate) throw new Error(checkRangeDate);
+  }
 };
 
 module.exports = { revenue, topProduct };
